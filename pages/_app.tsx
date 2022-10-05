ed5f962bbb69c41da0a94c5ac24e71823b03a150
bensultan1985 <bensultan1985@gmail.com>
2022-10-04 19:06:49 -0700

    initialize project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,50 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import Head from "next/head";
+import {
+  AppShell,
+  Container,
+  Footer,
+  Header,
+  MantineProvider,
+  Title,
+} from "@mantine/core";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: "light",
+        }}
+      >
+        <AppShell
+          fixed={false}
+          padding="lg"
+          header={
+            <Header height="auto" fixed px="xl">
+              <Title>Cross Off</Title>
+            </Header>
+          }
+          footer={<Footer height="auto" fixed children={"test"}></Footer>}
+        >
+          <Container>
+            <Component {...pageProps} />
+          </Container>
+        </AppShell>
+      </MantineProvider>
+    </>
+  );
 }
-
-export default MyApp
